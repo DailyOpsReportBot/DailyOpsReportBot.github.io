49560e3648f51d65c7dac8bb7723810d85c6678c
DailyOpsReportBot <dailyopsreportbot@gmail.com>
2026-07-02 19:55:35 -0400

    Update download-codes.js to remove old hash entry
    Removed an old hash entry for the Client Code.

--- assets/js/download-codes.js
+++ assets/js/download-codes.js
@@ -1,8 +1,5 @@
 window.DORB_DOWNLOAD_CODE_HASHES = [
-  {
-    "label": "Client Code",
-    "hash": "4c164475593330ca3265bb363716f9edf2fd0a039b29c851a49b6252725393f9"
-  },
+  { label: "Client Code", hash: "1e9e93056967252227a078d71b80f637ea16501f4df00e49849b129a98461d57" },
   {
     "label": "Client Code",
     "hash": "fadee69bc0f745f82cf72c71faf5c2424ae8fb0388431433a045066de11d4a55"
