0503790aee993168014b7bcb27d30fb6a768637d
DailyOpsReportBot <dailyopsreportbot@gmail.com>
2026-07-02 21:37:00 -0400

    Add new client code hash to download-codes.js

--- assets/js/download-codes.js
+++ assets/js/download-codes.js
@@ -8,5 +8,6 @@ window.DORB_DOWNLOAD_CODE_HASHES = [
   { label: "Client Code", hash: "e3c53babade5d90329b9ac43505aee1c309de03c984d3c26c8fd0d6b89dd9059" }, // DORB-N3WU-ZRTX-PN8P
   { label: "Client Code", hash: "f6dc8afb400e0b3c24713d9eb4d3fc80432ecc3cdb615562a5ffe871b7b39c16" }, // DORB-UYLG-KGUM-FCDE
   { label: "Client Code", hash: "4579fa270a899e9e017cea067479d1f3411dc5fb461dd127bc4b1c0da1c6bcb4" }, // DORB-GNX8-2QW2-G38M
-  { label: "Client Code", hash: "3c9d7e493785ea549c89de8c151bf623955b1a5616b96b967d758192418256e0" } // DORB-GBM5-SESG-JLNW
+  { label: "Client Code", hash: "3c9d7e493785ea549c89de8c151bf623955b1a5616b96b967d758192418256e0" }, // DORB-GBM5-SESG-JLNW
+  { label: "Client Code", hash: "e50c7a8d61fa411c321768194852225c09f53f2cd59b10d4451bfd525c7285de" } // DORB-8JX8-324F-V4YK
 ];
